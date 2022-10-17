2f7dbcd01e63e19ffba5f962c5989b10bf6dd5bb
Yanuo Ma <myn508@hotmail.com>
2022-10-17 19:17:57 -0400

    Rebuild git history to make it clear

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,40 +1,87 @@
-"""
-Test cases for YourResourceModel Model
-
-"""
+\
 import os
 import logging
 import unittest
+from werkzeug.exceptions import NotFound
 from service.models import Product, DataValidationError, db
+from service import app
+from tests.factories import ProductFactory
 
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/testdb"
+)
 ######################################################################
-#  <your resource name>   M O D E L   T E S T   C A S E S
+#  P R O D U C T   M O D E L   T E S T   C A S E S
 ######################################################################
-class TestYourResourceModel(unittest.TestCase):
-    """ Test Cases for YourResourceModel Model """
+# pylint: disable=too-many-public-methods
+
+
+class TestProductModel(unittest.TestCase):
+    """Test Cases for Product Model"""
 
     @classmethod
     def setUpClass(cls):
-        """ This runs once before the entire test suite """
-        pass
+        """This runs once before the entire test suite"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        Product.init_db(app)
 
     @classmethod
     def tearDownClass(cls):
-        """ This runs once after the entire test suite """
-        pass
+        """This runs once after the entire test suite"""
+        db.session.close()
 
     def setUp(self):
-        """ This runs before each test """
-        pass
+        """This runs before each test"""
+        db.session.query(Product).delete()  # clean up the last tests
+        db.session.commit()
 
     def tearDown(self):
-        """ This runs after each test """
-        pass
+        """This runs after each test"""
+        db.session.remove()
 
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
 
-    def test_XXXX(self):
-        """ It should always be true """
-        self.assertTrue(True)
+    def test_find_product(self):
+        """It should Find a Product by ID"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        logging.debug(products)
+        # make sure they got saved
+        self.assertEqual(len(product.all()), 5)
+        # find the 2nd product in the list
+        product = Product.find(products[1].id)
+        self.assertIsNot(product, None)
+        self.assertEqual(product.name, products[1].name)
+        self.assertEqual(product.description, products[1].description)
+        self.assertEqual(product.price, products[1].price)
+
+    def test_find_by_name(self):
+        """It should Find a Product by Name"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
+
+    def test_find_or_404_found(self):
+        """It should Find or return 404 not found"""
+        products = ProductFactory.create_batch(3)
+        for product in products:
+            product.create()
+
+        product = product.find_or_404(products[1].id)
+        self.assertIsNot(product, None)
+        self.assertEqual(product.id, products[1].id)
+        self.assertEqual(product.name, products[1].name)
+        self.assertEqual(product.description, products[1].description)
+        self.assertEqual(product.price, products[1].price)
